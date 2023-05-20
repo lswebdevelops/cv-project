d1c152098a013fc6f1e0b440c5d76cb75a85e5a1
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-20 15:43:48 -0300

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import MyPage from './components/MyApp';
+import Footer from './components/Footer'
 
 
 function App() {
   return (
     <div className="App">
       <MyPage />
-   
+      <Footer />
     </div>
   );
 }
